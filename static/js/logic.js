9296c05348ce219adfec68ff395dd1db988bd300
Angie Gomez <angie@Angies-MacBook-Air.local>
2019-06-18 13:08:04 -0700

    heatmap pseudocode

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,17 +2,21 @@
 var arena_layer;
 
 d3.json("/venue_coords").then(function(response){
+  // list for arena markers
   var arena_markers = [];
   for (var i=0; i < response.length; i++) {
+    // coordinates for each arena
     var coords = [response[i].latitude, response[i].longitude];
+    // push each marker to the list
     arena_markers.push(L.marker(coords, {
       draggable: false,
     }).bindPopup("<h4>" + response[i].venue + "</h4>"));
   }
+  // add markers to the layer
   arena_layer = L.layerGroup(arena_markers);
 })
 
-
+// light map layer
 var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
@@ -20,6 +24,7 @@ var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?ac
   accessToken: API_KEY
 });
 
+// outdoor map layer
 var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
@@ -27,6 +32,7 @@ var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png
   accessToken: API_KEY
 });
 
+// satellite map layer
 var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
@@ -41,20 +47,24 @@ function read_route(route) {
     var heat_array = [];
     
     for (var i = 0; i < data.length; i++) {
+      // push all crime coordinates to the list
       heat_array.push(data[i]);
     }
 
+    // add the crime coordinates to a heat layer
     var heat = L.heatLayer(heat_array, {
       radius: 20,
       blur: 35
     });
 
+    // map formats
     var baseMaps = {
       Light: light,
       Outdoors: outdoors,
       Satellite: satellite
     };
     
+    // map layers
     var overlayMaps = {
       Arenas: arena_layer,
       HeatMap: heat
@@ -67,6 +77,7 @@ function read_route(route) {
       layers: [outdoors, heat, arena_layer]
     });
     
+    // adding all layers to myMap
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
   })
 };
